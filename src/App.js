c9d52ca6d1227dfc5e9096817ab856159988aec0
Mohammed Amar Fiaz (Financial Services) <mohammed-amar.fiaz@lloydsbanking.com>
2019-01-07 12:55:41 +0000

    WIP: need to fix store.subscribe

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import ButtonGroup from "./ButtonGroup";
+import { store } from "./store";
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
-  render() {
-    return <HelloWorld tech={this.state.tech} />;
-  }
+    render() {
+        return [
+            <HelloWorld key={1} tech={store.getState().tech} />,
+            <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
+        ];
+    }
 }
 
 export default App;
